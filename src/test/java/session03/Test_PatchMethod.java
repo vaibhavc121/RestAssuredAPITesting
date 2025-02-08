23206038400cba4d8a5ebafd44e867ae15f8445a
vaibhav <vaibhavc121@gmail.com>
2025-02-08 23:16:48 +0530

    8-2

--- src/test/java/session03/Test_PatchMethod.java
+++ src/test/java/session03/Test_PatchMethod.java
@@ -6,17 +6,17 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
-public class Test_PostMethod
+public class Test_PatchMethod
 {
 	@Test
 	public void test()
 	{
 		JSONObject jsonData = new JSONObject();
-		jsonData.put("name", "vaibhav");
-		jsonData.put("job", "QA");
+		jsonData.put("name", "vaibhav c");
+		jsonData.put("job", "SDET QA");
 
-		RestAssured.baseURI = "https://reqres.in/api/users";
+		RestAssured.baseURI = "https://reqres.in/api/users/13";
 		RestAssured.given().header("Content-type", "application/json").contentType(ContentType.JSON)
-				.body(jsonData.toJSONString()).when().post().then().statusCode(201).log().all();
+				.body(jsonData.toJSONString()).when().patch().then().statusCode(200).log().all();
 	}
 }
