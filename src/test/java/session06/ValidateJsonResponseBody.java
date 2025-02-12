eb3a6ed248b66ccd51c67c7fb0f4d16347d8b2dc
vaibhav <vaibhavc121@gmail.com>
2025-02-12 23:58:26 +0530

    12-2

---
+++ src/test/java/session06/ValidateJsonResponseBody.java
@@ -0,0 +1,36 @@
+package session06;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+public class ValidateJsonResponseBody
+{
+	@Test
+	public void UserListResponseBody()
+	{
+		// get requestspecification reference
+		RequestSpecification requestSpec = RestAssured.given();
+
+		// specify base URI & base path
+		requestSpec.baseUri("https://reqres.in/");
+		requestSpec.basePath("api/users?page=2");
+
+		// create/perform get request
+		Response response = requestSpec.get();
+
+		// read response body
+		ResponseBody responseBody = response.getBody();
+		String responseString = responseBody.asString();
+		System.out.println("response body: " + responseString);
+
+		// check for presense of george in response body
+		Assert.assertEquals(responseString.contains("George"), true);
+
+	}
+
+}
